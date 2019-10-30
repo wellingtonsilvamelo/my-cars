7a0a8aec54ede472c6fb775489d484cd628da1ab
wellington.melo <wellington.melo@CPX-NIF3W4MHANW.dir.svc.accenture.com>
2019-10-30 20:59:23 -0300

    Fix

--- src/main/java/com/melo/wellington/application/MyCarsApplication.java
+++ src/main/java/com/melo/wellington/application/MyCarsApplication.java
@@ -3,11 +3,13 @@ package com.melo.wellington.application;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 import com.melo.wellington.application.util.Util;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MyCarsApplication {	
 	
 	public static void main(String[] args) {
